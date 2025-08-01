1662ce6f954f6eb38cb3f2ff707f5687b4ebb69c
Nitin Shirsat <nitin.shirsat@pubmatic.com>
2025-08-01 20:16:03 +0530

    Update endpoint to actual after testing

--- modules/pubmaticRtdProvider.js
+++ modules/pubmaticRtdProvider.js
@@ -39,8 +39,7 @@ export function ConfigJsonManager() {
    */
   async function fetchConfig(publisherId, profileId) {
     try {
-      // const url = `${CONSTANTS.ENDPOINTS.BASEURL}/${publisherId}/${profileId}/${CONSTANTS.ENDPOINTS.CONFIGS}`;
-      const url = `https://hbopenbid.pubmatic.com/yieldModuleConfigApi`;
+      const url = `${CONSTANTS.ENDPOINTS.BASEURL}/${publisherId}/${profileId}/${CONSTANTS.ENDPOINTS.CONFIGS}`;
       const response = await fetch(url);
 
       if (!response.ok) {
